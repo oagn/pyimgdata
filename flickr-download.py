21e961262c94dd0508db70ab7e5cd69e27ed3cc4
Agnethe Seim Olsen <olsenas@cardiff.ac.uk>
2023-08-07 12:36:41 +0100

    Convert image to RGB before saving to file

--- flickr-download.py
+++ flickr-download.py
@@ -94,7 +94,6 @@ class FlickrImageDownload:
 
         if int(license) in self.config_license_allowed and url:
             image, h = self.load_image(url)
-            
             if image:
                 return image, url
             else:
@@ -212,6 +211,8 @@ class FlickrImageDownload:
                 self.get_metadata(url, photo, img)
                 if path:
                 #    img = self.process_image(img, path)
+                    # converting image to RGB , basically removing transparent backgrounds
+                    img = img.convert("RGB")
                     img.save(path)
             
             if self.track_progress():
